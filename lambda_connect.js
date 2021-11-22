b0e1f8c66deedb912e597574f2608783113f5d52
khamidi <kousha1367@gmail.com>
2021-11-21 20:10:15 -0800

    fix: running prettier

--- lambda_connect.js
+++ lambda_connect.js
@@ -1,29 +1,28 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 var dynamoClient = new AWS.DynamoDB.DocumentClient();
 var table = "chat";
 
 // This function will be called once a wss connection is established.
 // It will add the connection_id to a dynamoDB table "chat"
 exports.handler = async (event) => {
-    const connectionId = event.requestContext.connectionId
-    console.log("got event: ", event)
+  const connectionId = event.requestContext.connectionId;
+  console.log("got event: ", event);
 
-    const params = {
-        Item: {
-            connection_id: connectionId
-        },
-        TableName: table
+  const params = {
+    Item: {
+      connection_id: connectionId,
+    },
+    TableName: table,
+  };
+
+  try {
+    const data = await dynamoClient.put(params).promise();
+    return {
+      statusCode: 200,
     };
-
-    try {
-        const data = await dynamoClient.put(params).promise();
-         return {
-            statusCode: 200
-            };
-    } catch(err){
-        return {
-            statusCode: 500
-            };
-    }
-
+  } catch (err) {
+    return {
+      statusCode: 500,
+    };
+  }
 };
--- lambda_default.js
+++ lambda_default.js
@@ -1,52 +1,52 @@
-var AWS = require('aws-sdk');
-var http = require('http');
+var AWS = require("aws-sdk");
+var http = require("http");
 var dynamoClient = new AWS.DynamoDB.DocumentClient();
 var table = "chat";
 
-const APIHost = process.env.APIGatewayEndpoint
+const APIHost = process.env.APIGatewayEndpoint;
 
 // This function is being called whenever there is a new message is recieaved.
-// It will get the list of all connectionIds from dynamoDB table chat and then
+// It will get the list of all connectionIds from dynamoDB table "chat" and then
 // uses apiGateWay @connections to braodcast the message.
 // more info: https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-how-to-call-websocket-api-connections.html
 exports.handler = async (event) => {
-    // Get list of clients.
-    const params = {
-        TableName: table
-    };
-    var apig = new AWS.ApiGatewayManagementApi(
-    	{
-    		region: "us-east-2",
-    		endpoint: APIHost
-    	});
+  // Get list of clients.
+  const params = {
+    TableName: table,
+  };
+  var apig = new AWS.ApiGatewayManagementApi({
+    region: "us-east-2",
+    endpoint: APIHost,
+  });
 
-    var clients = {}
-    try {
-        clients = await dynamoClient.scan(params).promise();
-    } catch(err){
-        console.log("got error: ", err)
-        return {
-            statusCode: 500
-        }
-    }
-
-    // forward the message to all clients one by one.
-    for (let i=0; i< clients.Items.length; i++){
-        let connectionId = clients.Items[i].client_id
-		try {
-			await apig.postToConnection({
-				ConnectionId: connectionId,
-				Data: JSON.stringify(event),
-			}).promise();
-
-			console.log("request is done")
-		} catch(err) {
-			console.log("got error forwarding message: ", err)
-		}
-
-    }
-
+  var clients = {};
+  try {
+    clients = await dynamoClient.scan(params).promise();
+  } catch (err) {
+    console.log("got error: ", err);
     return {
-        statusCode: 200
+      statusCode: 500,
+    };
+  }
+
+  // forward the message to all clients one by one.
+  for (let i = 0; i < clients.Items.length; i++) {
+    let connectionId = clients.Items[i].connection_id;
+    try {
+      await apig
+        .postToConnection({
+          ConnectionId: connectionId,
+          Data: JSON.stringify(event),
+        })
+        .promise();
+
+      console.log("request is done");
+    } catch (err) {
+      console.log("got error forwarding message: ", err);
     }
+  }
+
+  return {
+    statusCode: 200,
+  };
 };
